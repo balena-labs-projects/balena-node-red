b323bd6e2be8e1198916fc2f8f33dfd516f67674
janvda <jan@Jans-MacBook-Pro.local>
2019-10-03 23:08:31 +0200

    -

--- pigpiod/start.sh
+++ pigpiod/start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo "Starting daemon pigpiod ..."
+#echo "Starting daemon pigpiod ..."
 
 # for pigpiod options see http://abyz.me.uk/rpi/pigpio/pigpiod.html
 #    option -g will run the deamon in the foreground.
 #    option -a 1 should assure that we are not using GPU memory.
 pigpiod -g -a 1
 
-echo "... daemon stopped unexpectedly."
+#echo "... daemon stopped unexpectedly."
