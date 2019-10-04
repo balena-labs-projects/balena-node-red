6cb792db8ed4b702b7fc5f1c39eec65b850d5a25
janvda <jan@Jans-MBP.lan>
2019-10-05 00:19:31 +0200

    move balena flow to correct location

--- node-red/app/start.sh
+++ node-red/app/start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Make the flow balena_flows.json available in the user library
-mkdir -p /data/node-red/user/lib/flows || true
-cp /usr/src/app/balena_flows.json /data/node-red/user/lib/flows/
+mkdir -p /data/node-red/user || true
+cp /usr/src/app/balena_flows.json /data/node-red/user/
 
 # Start app
 DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket node-red --settings /usr/src/app/settings.js
