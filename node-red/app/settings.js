2068d147315f7b670381fecfcf424c12f697443e
janvda <jan@Jans-MBP.lan>
2019-10-04 23:57:26 +0200

    assured the flows are properly setup

--- node-red/app/settings.js
+++ node-red/app/settings.js
@@ -48,7 +48,7 @@ module.exports = {
     debugMaxLength: 1000,
 
     // The file containing the flows. If not set, it defaults to flows_<hostname>.json
-    //flowFile: 'flows.json',
+    flowFile: 'balena_flows.json',
 
     // To enabled pretty-printing of the flow within the flow file, set the following
     //  property to true:
