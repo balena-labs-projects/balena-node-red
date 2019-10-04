f386cf4e739345d90f8176ddf65a1dc5f306d959
janvda <jan@Jans-MBP.lan>
2019-10-05 00:28:13 +0200

    don't overwrite the balena flows file

--- node-red/app/start.sh
+++ node-red/app/start.sh
@@ -2,7 +2,12 @@
 
 # Make the flow balena_flows.json available in the user library
 mkdir -p /data/node-red/user || true
-cp /usr/src/app/balena_flows.json /data/node-red/user/
+
+# only copy the flow balena_flows.json if it doesn't exist as we don't want to overwrite any
+# changes made via the node-red editor.
+if [ ! -f /data/node-red/user/balena_flows.json ]; then
+   cp /usr/src/app/balena_flows.json /data/node-red/user/balena_flows.json
+fi
 
 # Start app
 DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket node-red --settings /usr/src/app/settings.js
