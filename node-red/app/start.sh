0256fb972662771e0e8dfce9e6645e435666b420
janvda <jan@Jans-MBP.lan>
2019-10-05 00:06:01 +0200

    correction

--- node-red/app/start.sh
+++ node-red/app/start.sh
@@ -2,7 +2,7 @@
 
 # Make the default flows available in the user library
 mkdir -p /data/node-red/user/lib/flows || true
-cp /usr/src/app/flows/* /data/node-red/user/lib/flows/
+cp /usr/src/app/balena_flows.json /data/node-red/user/lib/flows/
 
 # Start app
 DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket node-red --settings /usr/src/app/settings.js
