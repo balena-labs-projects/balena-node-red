83bd513444680943798ed96947c9c45579721c53
janvda <jan@Jans-MBP.lan>
2019-10-05 00:51:44 +0200

    put flow in correct folder

--- node-red/app/start.sh
+++ node-red/app/start.sh
@@ -6,7 +6,7 @@ mkdir -p /data/node-red/user || true
 # only copy the flow balena_flows.json if it doesn't exist as we don't want to overwrite any
 # changes made via the node-red editor.
 if [ ! -f /data/node-red/user/balena_flows.json ]; then
-   cp /usr/src/app/balena_flows.json /data/node-red/user/balena_flows.json
+   cp /usr/src/app/flow/balena_flows.json /data/node-red/user/balena_flows.json
 fi
 
 # Start app
