582ec5902455fb4e41a29bfc5392244f4d2873ff
janvda <jan@Jans-MBP.lan>
2019-10-05 09:53:20 +0200

    adding extra check logic

--- pigpiod/start.sh
+++ pigpiod/start.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-echo "Starting daemon pigpiod ..."
+echo "Checking who is listening/bound to port 8888 ..."
+netstat -tlep | grep 8888
+if [ "$?" = 0 ]; then
+   echo "Some process is still listening/bound to that port"
+   echo "This process needs to be stopped first"
+   echo "Sleeping for 20 sec - we hope that this releases the bound TCP port 8888 ..."
+   sleep 20
+   echo "... sleeping finished"
+else
+   echo "OK, no processing is listing to port 8888"
+fi
 
+echo "Starting daemon pigpiod ..."
 # for pigpiod options see http://abyz.me.uk/rpi/pigpio/pigpiod.html
 #    option -g will run the deamon in the foreground.
 #    option -a 1 should assure that we are not using GPU memory.
