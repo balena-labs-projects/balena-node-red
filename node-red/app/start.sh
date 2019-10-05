2b75d2fdccde573c8508fbdc9858fa0ee3bf7dc0
janvda <jan@Jans-MBP.lan>
2019-10-05 09:40:46 +0200

    add extra logging

--- node-red/app/start.sh
+++ node-red/app/start.sh
@@ -6,7 +6,10 @@ mkdir -p /data/node-red/user || true
 # only copy the flow balena_flows.json if it doesn't exist as we don't want to overwrite any
 # changes made via the node-red editor.
 if [ ! -f /data/node-red/user/balena_flows.json ]; then
+   echo "Copying balena_flows.json from repository to folder /data/node-red/user"
    cp /usr/src/app/flow/balena_flows.json /data/node-red/user/balena_flows.json
+else
+    echo "NOT copying balena_flows.json from repository to folder /data/node-red/user as this flow already exists in that folder"
 fi
 
 # Start app
