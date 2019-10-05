98023589a1fb5f446af54cef98f2ebea0535dc91
janvda <jan@Jans-MBP.lan>
2019-10-05 09:44:16 +0200

    logging improvement

--- node-red/app/start.sh
+++ node-red/app/start.sh
@@ -9,7 +9,7 @@ if [ ! -f /data/node-red/user/balena_flows.json ]; then
    echo "Copying balena_flows.json from repository to folder /data/node-red/user"
    cp /usr/src/app/flow/balena_flows.json /data/node-red/user/balena_flows.json
 else
-    echo "NOT copying balena_flows.json from repository to folder /data/node-red/user as this flow already exists in that folder"
+    echo "NOT copying balena_flows.json from repository to folder /data/node-red/user as this flow already exists in that folder !!"
 fi
 
 # Start app
